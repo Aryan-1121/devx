a224c9d24e4d4df846758f21d345ce393db49c85
=Aryan-1121 <aryanmaurya879@gmail.com>
2023-12-26 18:54:36 +0530

    JS functions for Strings

---
+++ week1/1.3/strings.js
@@ -0,0 +1,148 @@
+// String handbook
+
+// String: length, indexOf(), lastIndexOf(), slice(), substring(), replace(),
+// split(), trim(), toUpperCase(), toLowerCase(), etc.
+
+// Run each function to see the output, play and learn by doing.
+
+// Length
+
+// function getLength(str) {
+//     console.log("Original String:", str);
+//     console.log("Length:", str.length);
+//   }
+
+//   const userName = "Aryan" ;
+//   getLength(userName);
+
+
+
+//      indexOf
+
+// function findIndexOf(str, target) {
+//     console.log("Original String:", str);
+//     // console.log("Index:", str.indexOf(target));
+//     console.log("Index:", str.lastIndexOf(target));
+
+//   }
+
+
+// findIndexOf("Hello World new World", "World");
+
+
+//                 // slice
+// function getSlice(str, start, end) {
+//     console.log("Original String:", str);
+//     // start index is included and end index is not included
+//     console.log("After slice:", str.slice(start, end));
+//   }
+
+
+//   getSlice("Hello World", 0, 7);
+
+// let ans ="Aryan maurya".slice(0,4);
+// console.log(ans);
+
+
+// // substring
+// function getSubstring(str, start, end) {
+//   console.log("Original String:", str);
+//   console.log("After substring:", str.substring(start, end));
+
+// const value = "super man";
+// let ans = value.slice(2,7);
+// let ans2 =value.substr(2,7);
+
+// console.log(ans);
+
+// console.log(ans2);
+
+
+
+// // slice
+// function getSlice(str, start, end) {
+//     console.log("Original String:", str);
+//     console.log("After slice:", str.slice(start, end));
+//   }
+//   getSlice("Hello World", 1, 4);
+
+
+
+// // replace
+// function replaceString(str, target, replacement) {
+//     console.log("Original String:", str);
+//     console.log("After replace:", str.replace(target, replacement));
+//   }
+//   replaceString("Hello World", "World", "JavaScript");
+
+
+
+
+//          splitString  -> it splits my string to array based on the delimitter
+
+//        // split
+// function splitString(str, separator) {
+//     console.log("Original String:", str);
+//     console.log("After split:", str.split(separator));
+//   }
+//   splitString("Hi may name is super man"," " );
+
+
+
+
+
+// const userName ="super";
+// let ans = userName.split("");
+// console.log(ans)
+
+
+
+
+
+// trim         -> trims out the extra space from beggining and end (not from mid )
+// function trimString(str) {
+//     console.log("Original String:", str);
+//     console.log("After trim:", str.trim());
+//   }
+//   trimString(" Hello World i   am super man   ");
+
+
+
+// // toUpperCase
+// function toUpper(str) {
+//     console.log("Original String:", str);
+//     console.log("After toUpperCase:", str.toUpperCase());
+//   }
+//   toUpper("Hello World");
+  
+//   // toLowerCase
+//   function toLower(str) {
+//     console.log("Original String:", str);
+//     console.log("After toLowerCase:", str.toLowerCase());
+//   }
+//   toLower("Hello World");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
