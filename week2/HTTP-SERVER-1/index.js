7ecf7965042bc2a2f4c8b87423a9703e0f553f0c
=Aryan-1121 <aryanmaurya879@gmail.com>
2024-01-12 02:31:03 +0530

    reading headers/body/query-param from http req.

--- week2/HTTP-SERVER-1/index.js
+++ week2/HTTP-SERVER-1/index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 
+const bodyParser = require("body-parser");
+//  we can use body-parser, the body-parser module enables us to parse incoming request bvodies in a middleware. Express.js server needs to know what type of data you're sending over the nerwork, so it knows how to parse it (eg. app.use(bodyParser.json()); )
+
 const port =3000;
 
 
@@ -65,6 +68,68 @@ app.post("/check", (req, res)=>{
         //   }
     });
 })
+
+
+
+//      how to get body from post req. 
+
+
+
+// const a = bodyParser.json();
+
+app.use(bodyParser.json());
+
+app.post("/body", function(req,res){
+// console.log(req.body);          // there is no direct function to get body therfore we use body-parser (its also a library just like express is a library (install it in similar way -> npm install body-parser))
+console.log(req.headers);
+
+
+
+console.log("-----------BODY----------");
+
+console.log(req.body);
+
+
+// we need to do res.send or res.json or res.{something} otherwise our postman will keep waiting for some body 
+res.send(req.body);
+
+    // bodyParser.json();
+})
+
+
+
+
+app.post("/query-param",(req, res)=>{
+
+
+    msg = req.query.message;
+    // query is the provided function , "message" is the exact name from the query in the url 
+    console.log(msg)
+
+    res.send(req.query.message);
+})
+
+
+//  npm -> node package manager - used to fetch external library or something from internet to our local machine (download)
+// what is npx  ->  we can use npx in place of npm (it will not download the library  or something to local machine - its just one time thing)  eg. npx nodemon
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if you want to see your app hosted on localhost on soome other device on same wifi network then just use ip:{port no.}       eg- . -> 196.168.5.7:3000
+
+
 
 
 
