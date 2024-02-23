0b3f2382ed3a3335e23b7969a8e2a1a76a18f72c
=Aryan-1121 <aryanmaurya879@gmail.com>
2024-02-23 20:31:11 +0530

    JWT Authorization

---
+++ week3/4-Authentication-3.2/index.js
@@ -0,0 +1,117 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+
+
+const jwtPassword = "123$xyz";
+
+const app = express();
+
+app.use(express.json());
+
+//  array of objects 
+const ALL_USERS = [
+    {
+        username: "superman@gmail.com",
+        pw: "superr",
+        name: "clark"
+    },    
+    {
+        username: "joker@gmail.com",
+        pw: "1212",
+        name: "ledger"
+    },    
+    {
+        username: "hero@gmail.com",
+        pw: "abcdd",
+        name: "herooo"
+    }
+];
+
+
+function userExists(username, password){
+    // return true if provided username & password is present in in-memory-db (ALL_USERS)
+    let isPresent = false; 
+
+    for(let  i = 0; i<ALL_USERS.length; i++){
+        if(ALL_USERS[i].username == username && ALL_USERS[i].pw == password){
+            isPresent = true;
+            break; 
+        }
+    }
+
+    return isPresent; 
+
+
+}    
+
+
+app.post("/signin",(req, res) =>{
+
+    const username = req.body.username;
+    const pw = req.body.password;
+
+    if(!userExists(username, pw)){
+        return res.status(403).json({
+            msg: "user doesn't existi in our in memory db"
+        });
+    }
+
+    // if user exists in our in memory db - then return a jwt 
+
+    var token = jwt.sign({username: username}, jwtPassword);
+
+    return res.json({
+        token
+    });
+});
+
+
+//  get all users except the given one 
+app.get("/users", function(req, res){
+    //  expects token in header 
+    const token = req.headers.authorization;
+
+    try{
+        const decoded = jwt.verify(token, jwtPassword);
+        const usernmae = decoded.username;
+        res.json({
+            user : ALL_USERS.filter((value)=>{
+                if(value.username == usernmae)
+                    return false ;
+                return true;
+            })
+        });
+    }
+    catch(err){
+        return res.status(403).json({
+            msg : " invalid tokenn !1"
+        });
+    }
+});
+
+app.listen(3000,()=>{
+    console.log(`App is listening to port 3000`);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
