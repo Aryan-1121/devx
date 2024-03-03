6478dd330404ef065f42e30c911a2f6b56ce73ff
=Aryan-1121 <aryanmaurya879@gmail.com>
2024-03-03 19:45:14 +0530

    httpified data ssaving to mongoDB

--- week3/5-Databases/index.js
+++ week3/5-Databases/index.js
@@ -7,12 +7,21 @@
 //     /users - header (jwt in authorization header)
 
 
+const express = require("express");
+const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
+
+
+const app = express();
+
+app.use(express.json());
+
+const jwtPassword = "abc$990";
 
 mongoose.connect("mongodb+srv://admin:devxat2024@cluster0.6grydmw.mongodb.net/userappnew");
 
 
-//  defining the schema
+//  STEP 1- defining the schema
 const User = mongoose.model('Users',
 {
     name: String,
@@ -21,23 +30,50 @@ const User = mongoose.model('Users',
 });
 
 
-//      first create in-memory user and then save 
-const user = new User(
-    {
-        name : "clarkk",
-        "email":"superman@gmail.com",
-        "password":"superr"
+
+app.post("/signup", async function(req, res){
+
+    const name = req.body.name; 
+    const username = req.body.email; 
+    const pass = req.body.password;
+
+    // check if user is already existing in db or not 
+
+    const isExisting = await User.findOne(
+        {
+            email : username
+        }
+    );
+
+    if(!isExisting){
+        //      first create in-memory user and then save 
+
+        const user = new User(
+        {
+            name : name,
+            "email":username,
+            "password":pass
+        });
+            
+        user.save();
+        res.json({
+            msg:"User created successfully"
+        });
+
     }
-);
 
-user.save();
+// else if user already esists in db ->
+res.status(400).send({
+    msg:"user with this email already exists, try another one !! "
+});
+
+});
 
 
 
-
-
-
-
+app.listen(3000, ()=>{
+    console.log("app listening at port 3000");
+});
 
 
 
