7c8f2eccf99517db4a19d4d9be45d941a3b7fca8
=Aryan-1121 <aryanmaurya879@gmail.com>
2024-03-17 21:56:17 +0530

    added input validation for api calls

--- week5/TODO-App/backend/index.js
+++ week5/TODO-App/backend/index.js
@@ -1,21 +1,46 @@
 
 const express = require("express");
 
+const {createTodo, updateTodo} = require("./types")
 const app = express();
 
 
 app.use(express.json())
 
 app.post("/todo", (req, res)=>{
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload)
+
+    if(!parsedPayload.success){
+        res.status(411).json({
+            msg: "wrong inputs"
+        });
+    }
+
+    // put in mongo db 
+
 
 })
 
-app.get("/todos", ()=>{
+app.get("/todos", (req,res)=>{
+
+    const id = req.params.id;
+
+
 
 })
 
 
 app.put("/complete", ()=>{
+
+    const updatePayload = req.body; 
+    const parsedPayload = updateTodo.safeParse(updatePayload);
+
+    if(!parsedPayload.success){
+        res.status(411).json({
+            msg: "wrong inputs"
+        });
+    }
 
 })
 
