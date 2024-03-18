361619932a200797b22300110a5aec03e2f328c5
=Aryan-1121 <aryanmaurya879@gmail.com>
2024-03-18 19:05:11 +0530

    done with backend apis

--- week5/TODO-App/backend/index.js
+++ week5/TODO-App/backend/index.js
@@ -1,13 +1,14 @@
 
 const express = require("express");
 
-const {createTodo, updateTodo} = require("./types")
+const {createTodo, updateTodo} = require("./types");
+const { todo } = require("./db");
 const app = express();
 
 
 app.use(express.json())
 
-app.post("/todo", (req, res)=>{
+app.post("/todo", async (req, res)=>{
     const createPayload = req.body;
     const parsedPayload = createTodo.safeParse(createPayload)
 
@@ -18,20 +19,33 @@ app.post("/todo", (req, res)=>{
     }
 
     // put in mongo db 
+    await todo.create({
+        title: createPayload.title,
+        description: createPayload.description
+        completed: false
+    })
+
+    res.json({
+        msg: "Todo Created"
+    })
+
+
 
 
 })
 
-app.get("/todos", (req,res)=>{
-
-    const id = req.params.id;
+app.get("/todos", async (req,res)=>{
 
 
+    const todos = await todo.find({});          //need to await since its a db call
+    res.json({
+        todos
+    })
 
 })
 
 
-app.put("/complete", ()=>{
+app.put("/complete", async(req,res)=>{
 
     const updatePayload = req.body; 
     const parsedPayload = updateTodo.safeParse(updatePayload);
@@ -41,6 +55,16 @@ app.put("/complete", ()=>{
             msg: "wrong inputs"
         });
     }
+
+    await todo.update({
+        _id: req.body.id
+    },{
+        completed: true
+
+    });
+    res.json({
+        msg: " Todo marked as done !!"
+    })
 
 })
 
