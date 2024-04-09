ae38ca2db2b1d7c0475e4def757881c559af9c11
=Aryan-1121 <aryanmaurya879@gmail.com>
2024-04-10 03:43:54 +0530

    useMemo usecase

--- week6/3-React-hooks/src/App.jsx
+++ week6/3-React-hooks/src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useMemo, useState } from "react"
 
 
 // function App() {
@@ -90,60 +90,131 @@ import { useEffect, useState } from "react"
 
 
 
+// function App(){
+
+//   const [button, setbutton] = useState(1);
+
+
+
+//   return <div>
+//     <button onClick={()=>{
+//       setbutton(1);
+//     }}>1</button>
+//     <button onClick={()=>{
+//       setbutton(2);
+//     }}>2</button>
+//     <button onClick={()=>{
+//       setbutton(3);
+//     }}>3</button>
+    
+//     <button onClick={()=>{
+//       setbutton(4);
+//     }}>4</button>
+//     <button onClick={()=>{
+//       setbutton(5);
+//     }}>5</button>
+
+//     <Todo id={button} />
+//   </div>
+// }
+
+
+
+
+// function Todo({ id }) {
+// console.log(id);
+//     const [todo, setTodo] = useState([]);
+  
+  
+//     useEffect(() => {
+  
+//       fetch("https://sum-server.100xdevs.com/todo?id="+ id)
+//         .then(async (res) => {
+//           const json = await res.json();
+//           setTodo(json.todo);
+//         })
+  
+//     }, [id]);
+  
+  
+//     return <div>
+//       <h1>
+//         {todo.title}
+//       </h1>
+//         {todo.description}
+//     </div>
+//   }
+
+
+
+
+
+
+      //      USE MEMEO
+
+    
+
+//   function App(){
+//     const [counter, setCounter] = useState(0); 
+//     const [inputValue, setInputValue] = useState(1);
+
+//     let count =0; 
+//     for(let i =1; i<=inputValue; i++){
+//       count+=i;
+//     }
+
+//     return <div>
+//       <input onChange={(e)=>{
+//         setInputValue(e.target.value);
+//       }}
+//       placeholder="find sum from 1 to n "></input>
+//       <br />
+//       Sum fromo 1 to {inputValue} is {count}
+//       <br />
+
+
+// {/* when we click tis button it causes re-render that will make above code run too, even if above thing is not changed  */}
+//       <button onClick={()=>{
+//         setCounter(counter +1);
+//       }}>Counter({counter})</button>
+//     </div>
+// }
+
+
+
+
+
+
 function App(){
+  const [counter, setCounter] = useState(0); 
+  const [inputValue, setInputValue] = useState(1);
 
-  const [button, setbutton] = useState(1);
-
-
+  let count = useMemo(()=>{
+    let finalCount =0;
+    for(let i =1; i<=inputValue; i++){
+      finalCount+=i;
+    }
+    return finalCount;
+  },[inputValue]) 
 
   return <div>
-    <button onClick={()=>{
-      setbutton(1);
-    }}>1</button>
-    <button onClick={()=>{
-      setbutton(2);
-    }}>2</button>
-    <button onClick={()=>{
-      setbutton(3);
-    }}>3</button>
-    
-    <button onClick={()=>{
-      setbutton(4);
-    }}>4</button>
-    <button onClick={()=>{
-      setbutton(5);
-    }}>5</button>
+    <input onChange={(e)=>{
+      setInputValue(e.target.value);
+    }}
+    placeholder="find sum from 1 to n "></input>
+    <br />
+    Sum fromo 1 to {inputValue} is {count}
+    <br />
 
-    <Todo id={button} />
+
+{/* when we click tis button it causes re-render that will make above code run too, even if above thing is not changed  */}
+    <button onClick={()=>{
+      setCounter(counter +1);
+    }}>Counter({counter})</button>
   </div>
-}
 
-
-
-
-function Todo({ id }) {
-console.log(id);
-    const [todo, setTodo] = useState([]);
-  
-  
-    useEffect(() => {
-  
-      fetch("https://sum-server.100xdevs.com/todo?id="+ id)
-        .then(async (res) => {
-          const json = await res.json();
-          setTodo(json.todo);
-        })
-  
-    }, [id]);
-  
-  
-    return <div>
-      <h1>
-        {todo.title}
-      </h1>
-        {todo.description}
-    </div>
   }
+
 
 
 
