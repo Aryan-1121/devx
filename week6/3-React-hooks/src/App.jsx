19eae9ccf48ccb88a6e1bddbc5141f2c4ade9b67
=Aryan-1121 <aryanmaurya879@gmail.com>
2024-04-10 02:26:54 +0530

    basic problem on useEffect

--- week6/3-React-hooks/src/App.jsx
+++ week6/3-React-hooks/src/App.jsx
@@ -13,54 +13,76 @@ import { useEffect, useState } from "react"
 // }
 
 
-function App(){
-  const [todos, setTodos] = useState([]);
-  useEffect(()=>{
-    fetch('https://sum-server.100xdevs.com/todos')
-    .then(async function(res){
-      const json = await res.json();
-      setTodos(json.todos);
-    })
-  }, [])      // empty dependency array means do this only once no matter how many times render happen 
+// function App(){
+//   const [todos, setTodos] = useState([]);
+//   useEffect(()=>{
+//     fetch('https://sum-server.100xdevs.com/todos')
+//     .then(async function(res){
+//       const json = await res.json();
+//       setTodos(json.todos);
+//     })
+//   }, [])      // empty dependency array means do this only once no matter how many times render happen 
 
- //DEPENDENCY ARRAY - when should the callback function run 
- // DEPENDENCY ARRAY -> takes state variable as a input   
+//  //DEPENDENCY ARRAY - when should the callback function run 
+//  // DEPENDENCY ARRAY -> takes state variable as a input   
 
-//  if we don't use useEffect it will infinitely keep changing bcs setTodos will change and App will re-render again then again setTodos will get updated and so on ..
-  return(
-    <div>
-      {todos.map(todo => <Todo key={todo.id} title={todo.title} description={todo.description}/> ) }
-    </div>
-  )
+// //  if we don't use useEffect it will infinitely keep changing bcs setTodos will change and App will re-render again then again setTodos will get updated and so on ..
+//   return(
+//     <div>
+//       {todos.map(todo => <Todo key={todo.id} title={todo.title} description={todo.description}/> ) }
+//     </div>
+//   )
 
-  function Todo({title, description}){
-    return <div>
-      <h1> {title}</h1>
-      {description}
-    </div>
-  }
+//   function Todo({title, description}){
+//     return <div>
+//       <h1> {title}</h1>
+//       {description}
+//     </div>
+//   }
+
+// }
+
+
+
+
+
+// A Component that takes a todo id as input and fetches that todo from an endpoint and renders it 
+
+
+function App() {
+
+  return <div>
+    <Todo id={2} />
+  </div>
+
 
 }
 
 
 
+function Todo({ id }) {
+
+  const [todo, setTodo] = useState([]);
 
 
+  useEffect(() => {
+
+    fetch("https://sum-server.100xdevs.com/todo?id="+ id)
+      .then(async (res) => {
+        const json = await res.json();
+        setTodo(json.todo);
+      })
+
+  }, []);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+  return <div>
+    <h1>
+      {todo.title}
+    </h1>
+      {todo.description}
+  </div>
+}
 
 
 
