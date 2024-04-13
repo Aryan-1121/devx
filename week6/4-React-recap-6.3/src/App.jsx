39e1e7eeeef00c4d86606009b58b35b0be17ce6b
=Aryan-1121 <aryanmaurya879@gmail.com>
2024-04-13 22:37:18 +0530

    correct way for DOM manupulation: useRef

--- week6/4-React-recap-6.3/src/App.jsx
+++ week6/4-React-recap-6.3/src/App.jsx
@@ -1,20 +1,28 @@
-import { memo, useCallback, useEffect, useMemo, useState } from 'react'
+import { memo, useCallback, useEffect, useMemo, useRef, useState } from 'react'
 
 
 //    lets suppose we want to update/re-write the dom element which react already gave us 
-    // (ideally should not do this , but lets say we havea scenario which requires this )
-    // ideally we should just use useState and other hooks which monitor it 
+// (ideally should not do this , but lets say we havea scenario which requires this )
+// ideally we should just use useState and other hooks which monitor it 
 
+
+// using document.getElem... is wrong way of updating dom elemnts , react gives 'useRef' hook for it 
 function App() {
   const [income, setIncome] = useState(5000);
+  const divRef = useRef();
 
-    setTimeout(()=>{
-      document.getElementById('incomeDiv').innerHTML = 900;
+  useEffect(() => {
+
+    setTimeout(() => {
+      // document.getElementById('incomeDiv').innerHTML = 900;
+      console.log(divRef.current);
+      divRef.current.innerHTML = 800;
     }, 5000);
+  }, [])
 
-   return (
+  return (
     <div>
-      income is : <div id='incomeDiv'> { income }  </div>
+      income is : <div ref={divRef}> {income}  </div>
     </div>
   )
 }
