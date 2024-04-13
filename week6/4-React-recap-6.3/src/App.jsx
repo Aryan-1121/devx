d0bc5b2b4aeed8f1b11b15991c2ba7999a525bc5
=Aryan-1121 <aryanmaurya879@gmail.com>
2024-04-13 11:14:21 +0530

    problem with conventional react way of re-rendering

--- week6/4-React-recap-6.3/src/App.jsx
+++ week6/4-React-recap-6.3/src/App.jsx
@@ -1,17 +1,43 @@
 import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
-  // document.getElementbyId
 
-  console.log('this is from APp.jsx');      // whenever state var changes whole component re-renders
+const [cryptoData, setCryptoData] = useState({});
+const [bankData, setBankData] = useState({});
+
+// fetch('https://google.com', async(res) =>{
+//   const json = await res.json();
+//   // setBankData(json);
+//   // eg. let json data be -> {income : 200}
+//   setBankData({income : 200})
+// });
+
+// simulating fectch req. to a setTimeout otherwise since url is incorrect it will give error
+
+setTimeout( ()=>{
+  console.log('re-render form bank Data');
+
+  setBankData({income : 200});
+}, 3000);
+
+setTimeout(()=>{
+  console.log('re-render form Crypto Data');
+
+  setCryptoData({
+    returns: 100
+  });
+}, 1000);
+
+// PROBLEM   ->   each time setBankData and setCrptoData gets updated a whole App re-render occur then again these 2 gets updated then again whole app re-render and this goes on infinitely -> TO SOLVE THIS ISSUE WE HAVE {USEeFFECT}
+
+const incomeTax = (bankData.income + cryptoData.returns) * 0.3 ; 
+
+
+
+
   return (
     <div>
-      <button onClick={()=>{
-        setCount(count +1);
-      }}>
-        count -{'>'} {count}
-      </button>
+      your Income Tax returns are  : {incomeTax}
 
     </div>
   )
