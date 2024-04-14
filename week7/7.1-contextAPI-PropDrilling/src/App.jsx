d74f61574a1cff373be85d29638779bd2ced7ca1
=Aryan-1121 <aryanmaurya879@gmail.com>
2024-04-15 00:09:42 +0530

    Created 2 routes with common header

--- week7/7.1-contextAPI-PropDrilling/src/App.jsx
+++ week7/7.1-contextAPI-PropDrilling/src/App.jsx
@@ -2,14 +2,26 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Dashboard } from './components/Dashboard'
+import { Landing } from './components/Landing'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-    </>
+    <div>
+      <div>
+        <h1> thisi  is the top bar</h1>
+      </div>
+
+      <BrowserRouter>
+        <Routes>
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/' element={<Landing />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
