6fedb76f15b4eac6b17aafc13d379cc51d80b81b
=Aryan-1121 <aryanmaurya879@gmail.com>
2024-04-16 00:53:53 +0530

    Problem : Prop Drilling

--- week7/7.1-PropDriling/src/App.jsx
+++ week7/7.1-PropDriling/src/App.jsx
@@ -1,29 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+
+            //          PROP DRILLING IS A PROBLEM -> solution -contex api, state management (redux, recoil)
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-      <Count count={count}/>
-      <Buttons count={count} setCount={setCount} />
+      <Count count={count} setCount={setCount} />
+      {/* <Buttons count={count} setCount={setCount} /> */}
 
     </div>
   )
 }
 
+//  PROBLEM : -lets suppose Buttons component needs to get called from COunt component -> now here even Count component only needs 'count' as prop but now bcs Buttons component needs both 'count' as well as 'setCount' now Count component also need to take both state variable as prop => not good to look at, slightly not appealing to eye, there might be case when least common ancestor of a state variable is also very far from the actual component that needs it so all the component in b/w will have to pass down those state variable as props even when they nothing to do with it  
 
-
-function Count({ count }) {
+function Count({ count, setCount }) {
   return <div>
     {count}
+    <Buttons count={count} setCount={setCount} />
+
   </div>
 }
 
 
-function Buttons({ count, setCount}) {
+function Buttons ({ count, setCount}) {
   return <div>
 
     <button onClick={() => {
