89cebe1d2ba7869deef518c85e5c0d97f8ee2839
=Aryan-1121 <aryanmaurya879@gmail.com>
2024-04-15 16:17:00 +0530

    useNavigate hook

--- week7/7.1-contextAPI-PropDrilling/src/App.jsx
+++ week7/7.1-contextAPI-PropDrilling/src/App.jsx
@@ -1,21 +1,33 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom'
 import { Dashboard } from './components/Dashboard'
 import { Landing } from './components/Landing'
 
 function App() {
   const [count, setCount] = useState(0)
 
+
   return (
     <div>
-      <div>
-        <h1> thisi  is the top bar</h1>
+
+      <div style={{ background: 'black', color: 'white' }}>
+        <h1 > TOP BAR </h1>
       </div>
 
+      {/* <button onClick={() => {
+          // window.location.href = '/'      //this will reload whole file (get file from server ) (hard refresh the page), we dont want this -> SOLUTION: useNavigate()
+
+          navigate('/')
+        }} > Landing</button>
+
+        <button onClick={() => {
+          // window.location.href = '/dashboard'
+          navigate('/dashboard')
+        }}> Dashboard </button> */}
+
+
       <BrowserRouter>
+        <AppBar />
         <Routes>
           <Route path='/dashboard' element={<Dashboard />} />
           <Route path='/' element={<Landing />} />
@@ -23,6 +35,26 @@ function App() {
       </BrowserRouter>
     </div>
   )
+}
+
+
+const AppBar = () => {
+  const navigate = useNavigate();
+  //useNavigate expects to make sure this hook inside BrowserRouter
+  return <div>
+    <button onClick={() => {
+      // window.location.href = '/'      //this will reload whole file (get file from server ) (hard refresh the page), we dont want this -> SOLUTION: useNavigate()
+
+      navigate('/')
+    }} > Landing</button>
+
+    <button onClick={() => {
+      // window.location.href = '/dashboard'
+      navigate('/dashboard')
+    }}> Dashboard </button>
+
+
+  </div>
 }
 
 export default App
