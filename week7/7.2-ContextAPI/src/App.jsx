046211934edbf0a666bd57a1625f185f9aecf2cf
=Aryan-1121 <aryanmaurya879@gmail.com>
2024-04-18 17:48:00 +0530

    removed contextAPI logic, created count state var as an atom, replaced all useState hook logic with recoil

--- week7/7.2-ContextAPI/src/App.jsx
+++ week7/7.2-ContextAPI/src/App.jsx
@@ -1,57 +1,55 @@
-import { useState } from 'react'
-import { CountContext } from './context'
-import { useContext } from 'react'
 
+import { countAtom } from './store/atoms/count';
+import { RecoilRoot, useRecoilState, useRecoilValue } from "recoil";
 // =>  CONTEXT API  does not make rendering more perfomant  => NOOOOOOOOO
 //  => IT JUST MAKE CODE LOOK CLEANER (THAT IS GET RID OF PROP DRILLING)
 
 // TO SUMMARISE THE PROBLEM - > it solves prop drilling but still re-render the children which is not using the state variable which got updated
 
+// SOLUTION - state management library (recoil)
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div>
-      {/* <CountContext.Provider value={{count, setCount }}> */}
-      <CountContext.Provider value={count }>
-        <Count setCount={setCount}/>
-        {/* <Buttons count={count} setCount={setCount} /> */}
-      </CountContext.Provider>
+      {/* need to wrap everything which uses recoil stuff(state vars defined in atom) bbetween RecoilRoot */}
+      <RecoilRoot>
+
+        <Count />
+      </RecoilRoot>
     </div>
   )
 }
 
-
-function Count({ setCount }) {
+// now this component will not get re-render when not req. unlike earlier(will re-render only 1st time it gets rendered )
+function Count() {
   console.log('couter got re-render');
-  // PROBLEM : why does this compoonent gets re-render even though it is not using the state variable which is updating while clicking on the inc/dec button
   return <div>
     <CountRenderer  />
-    <Buttons setCount={setCount} />
+    <Buttons  />
 
   </div>
 }
 
 function CountRenderer() {
-  const count = useContext(CountContext);
+  // this component just need to use the value of count and not update so we will use useRecoilValue hook 
+  const count = useRecoilValue(countAtom);
   return <div>
     {count}
   </div>
 }
 
-function Buttons({ setCount }) {
-  const count = useContext(CountContext);
+function Buttons() {
+  // this component needs both thing count and setCOunt  => useRecoilState
+  const [count, setCount] = useRecoilState(countAtom);  
 
   return <div>
-
     <button onClick={() => {
       setCount(count + 1)
     }}> increase </button>
 
-
     <button onClick={() => {
       setCount(count - 1)
-
     }}> decrease  </button>  </div>
 }
 
---
+++ week7/7.2-ContextAPI/src/store/atoms/count.jsx
@@ -0,0 +1,15 @@
+// this could be count.js no need to specify jsx
+import { atom } from 'recoil'
+
+//  count related state variables will be defined here 
+
+export const countAtom = atom({
+    key: 'countAtom',
+    default: 0
+})
+
+
+
+
+
+
