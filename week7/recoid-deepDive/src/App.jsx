aaf0141f3d6eb639aef99c2e62a1b573d1f80c10
=Aryan-1121 <aryanmaurya879@gmail.com>
2024-04-27 23:57:42 +0530

    fetched global atom value in main component using useRecoilValue

--- week7/recoid-deepDive/src/App.jsx
+++ week7/recoid-deepDive/src/App.jsx
@@ -1,21 +1,29 @@
 import { useState } from 'react'
+import { RecoilRoot, useRecoilValue } from 'recoil'
+import { networkAtom } from './atoms'
 
-
+// I could have done same thing using useState
 function App() {
-  const [count, setCount] = useState(0)
+  return <RecoilRoot>
+    <MainApp />
+  </RecoilRoot>
+
+}
+
+function MainApp() {
+  const networkCount = useRecoilValue(networkAtom);
 
   return (
-
     <div>
       <button> Home</button>
 
 
-      <button> My Network </button>
+      <button> My Network({networkCount >= 100 ? '99+' : networkCount}) </button>
       <button> Jobs</button>
       <button> Messaging</button>
       <button> Notification</button>
 
-      
+
       <button> Me </button>
     </div>
   )
