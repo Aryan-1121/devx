c45c071a26a43329ce3fe459e33866f3f22fceb0
=Aryan-1121 <aryanmaurya879@gmail.com>
2024-04-27 23:47:23 +0530

    created 6 buttons and 4 atoms for global state

---
+++ week7/recoid-deepDive/src/atoms.js
@@ -0,0 +1,31 @@
+
+import { atom } from 'recoil';
+
+
+export const networkAtom = atom({
+    key : 'networkAtom',
+    default: 103
+});
+
+
+export const jobsAtom = atom({
+    key : 'jobsAtom',
+    default: 0
+});
+
+
+export const notificationAtom = atom({
+    key : 'notificationAtom',
+    default: 0
+});
+
+
+export const messagingAtom = atom({
+    key : 'messagingAtom',
+    default: 6
+});
+
+
+
+
+
