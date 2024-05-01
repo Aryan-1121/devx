2a4cc0984b6b52384ea725b95cdd38d36a6c99a0
Aryan <106900422+Aryan-1121@users.noreply.github.com>
2024-05-02 03:46:13 +0530

    An Array of VideoDetails which will be displayed using map function

--- week8/yt-frontend/src/components/VideoGrid.tsx
+++ week8/yt-frontend/src/components/VideoGrid.tsx
@@ -1,1 +1,126 @@
-aa
+import { VideoCard } from "./VideoCard"
+
+
+const VIDEOS = [{
+
+  title: 'here will come the title of the song',
+  channelName: 'Firebase',
+  views: '5.1M',
+  timestamp: '1 month ago',
+  channelImage: '/thumb.png',
+
+},
+{
+  title: 'here will video 12',
+  channelName: 'T series',
+  views: '5.1M',
+  timestamp: '1 month ago',
+  channelImage: '/thumb.png',
+},
+{
+
+  title: 'here will come video 3 of the song',
+  channelName: 'Firebase',
+  views: '5.1M',
+  timestamp: '1 month ago',
+  channelImage: '/thumb.png',
+
+},
+{
+
+  title: '4th video ',
+  channelName: 'Firebase',
+  views: '5.1M',
+  timestamp: '1 month ago',
+  channelImage: '/thumb.png',
+
+},
+{
+
+  title: 'last one ',
+  channelName: 'Firebase',
+  views: '5.1M',
+  timestamp: '1 month ago',
+  channelImage: '/thumb.png',
+
+}, {
+
+  title: 'last one ',
+  channelName: 'Firebase',
+  views: '5.1M',
+  timestamp: '1 month ago',
+  channelImage: '/thumb.png',
+
+}, {
+
+  title: 'last one ',
+  channelName: 'Firebase',
+  views: '5.1M',
+  timestamp: '1 month ago',
+  channelImage: '/thumb.png',
+
+}, {
+
+  title: 'last one ',
+  channelName: 'Firebase',
+  views: '5.1M',
+  timestamp: '1 month ago',
+  channelImage: '/thumb.png',
+
+}, {
+
+  title: 'last one ',
+  channelName: 'Firebase',
+  views: '5.1M',
+  timestamp: '1 month ago',
+  channelImage: '/thumb.png',
+
+}, {
+
+  title: 'last one ',
+  channelName: 'Firebase',
+  views: '5.1M',
+  timestamp: '1 month ago',
+  channelImage: '/thumb.png',
+
+}, {
+
+  title: 'last one ',
+  channelName: 'Firebase',
+  views: '5.1M',
+  timestamp: '1 month ago',
+  channelImage: '/thumb.png',
+
+}, {
+
+  title: 'last one ',
+  channelName: 'Firebase',
+  views: '5.1M',
+  timestamp: '1 month ago',
+  channelImage: '/thumb.png',
+
+},]
+
+
+
+export const VideoGrid = () => {
+  return <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+    {VIDEOS.map(video => <div>
+      <VideoCard
+        title={video.title}
+        channelName={video.channelName}
+        views={video.views}
+        timestamp={video.timestamp}
+        channelImage={video.channelImage}
+      />
+    </div>)}
+  </div>
+}
+
+
+
+
+
+
+
+
