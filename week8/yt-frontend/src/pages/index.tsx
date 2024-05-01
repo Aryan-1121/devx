f32b686d73a159e9896d90197e2505717279fcd3
Aryan <106900422+Aryan-1121@users.noreply.github.com>
2024-05-02 03:40:42 +0530

    Updated index.tsx

--- week8/yt-frontend/src/pages/index.tsx
+++ week8/yt-frontend/src/pages/index.tsx
@@ -1,12 +1,24 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+// import { VideoCard } from '../components/VideoCard'        // this works too 
+import { VideoCard } from '@/components/VideoCard'
+import { VideoGrid } from '../components/VideoGrid'
+import AppBar from '@/components/AppBar'
 
 export default function Home() {
   return (
-   <div>
-    hi there 
-   </div>
-  );
+    <div>
+      {/* <VideoCard
+        title={'here will come the title of the song'}
+        channelName={'Firebase'}
+        views={'5.1M'}
+        timestamp={'1 month ago'}
+        channelImage={'/thumb.png'}
+
+      /> */}
+      <AppBar />
+      <VideoGrid />
+    </div>
+
+
+  )
 }
