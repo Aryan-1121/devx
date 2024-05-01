5e5824950b648d0e7d42358d6fbdf876c293cbe9
Aryan <106900422+Aryan-1121@users.noreply.github.com>
2024-05-02 03:47:26 +0530

    Appbar will have YT logo, search bar and sign-in logo at the top

--- week8/yt-frontend/src/components/AppBar.tsx
+++ week8/yt-frontend/src/components/AppBar.tsx
@@ -1,1 +1,16 @@
-asdd
+import React from 'react'
+import SearchBar from './SearchBar'
+
+const AppBar = () => {
+  return (
+    <div className='flex justify-between'>
+      <div>YouTube</div>
+      <SearchBar />
+
+      <div>Sign-in</div>
+
+    </div>
+  )
+}
+
+export default AppBar
