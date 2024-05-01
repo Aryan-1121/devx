9f3a48f9696e74dae6a79207447590614ab0b0bb
Aryan <106900422+Aryan-1121@users.noreply.github.com>
2024-05-02 03:43:37 +0530

    this component contains video image, channelImage, videotitle,channelName,views, timestamp

--- week8/yt-frontend/src/components/VideoCard.tsx
+++ week8/yt-frontend/src/components/VideoCard.tsx
@@ -1,1 +1,32 @@
-ss
+
+
+export function VideoCard(props: any) {
+  return <div>
+    <img className="rounded-xl" src="/image.png" />
+
+
+    <div className="grid grid-cols-12 pt-2" >
+      <div className="col-span-1">
+        <img className="w-12 rounded-full" src={props.channelImage} />
+      </div>
+      <div className="col-span-11">
+        <div >
+          {/* here will come the title of the song */}
+          {props.title}
+        </div>
+        <div className="text-gray-500 ">
+          {/* Firebase */}
+          {props.channelName}
+        </div>
+        <div className="text-gray-500 text-sm">
+          {/* 5.1M views • 1 month ago */}
+          {props.views} views • {props.timestamp}
+        </div>
+      </div>
+    </div>
+  </div>
+
+}
+
+
+
