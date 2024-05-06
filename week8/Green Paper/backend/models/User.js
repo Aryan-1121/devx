0e7557f2ec27029196e61fe85963af1d0616766c
Aryan <aryanmaurya879@gmail.com>
2024-05-06 23:27:29 +0530

    updated gitignore

--- week8/Green Paper/backend/models/User.js
+++ week8/Green Paper/backend/models/User.js
@@ -2,14 +2,32 @@ import mongoose from "mongoose";
 
 
 const userSchema = new mongoose.Schema({
-  userName: String,
-  password: String,
-  fullName: String
+  fullName: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 30
+  },
+  userName: {
+    type: String,
+    required: true,
+    unique: true,
+    minLength: 3,
+    maxLength: 30
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6,
+  },
+
 })
 
 const User = mongoose.model('User', userSchema);
 
-
+module.exports = {
+  User,
+}
 
 
 
